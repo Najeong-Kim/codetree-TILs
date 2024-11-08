48c66f3ba534de8f46276728efddd1a74f8ff0d7
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-11-08 22:00:53 +0900

    Title: 함수를 이용한 369 게임, Time: 166ms, Memory: 18MB, Status: Passed - CodeTree

--- 241108/함수를 이용한 369 게임/369-games-using-functions.py
+++ 241108/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,7 +1,7 @@
 def is_correct_condition(n):
     result = False
     for i in range(len(str(n))):
-        if int(str(n)[i]) % 3 == 0:
+        if int(str(n)[i]) in [3, 6, 9]:
             result = True
     if (n % 3 == 0):
         result = True
