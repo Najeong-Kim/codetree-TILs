e53a3428b1b6e767ffd698bf4edc1f1833c3fbde
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-11-17 19:36:52 +0900

    Title: 원점으로부터의 거리, Time: 60ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 241117/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = []
+
+for i in range(1, n + 1):
+    x, y = map(int, input().split())
+    arr.append((x, y, i))
+
+arr.sort(key=lambda x: (abs(x[0]) + abs(x[1]), x[2]))
+
+for elem in arr:
+    _, _, i = elem
+    print(i)
