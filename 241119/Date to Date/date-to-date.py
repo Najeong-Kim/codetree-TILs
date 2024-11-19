d8ab17fa19e410518e62d135c49c68d92a76df93
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-11-19 23:55:33 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241119/Date to Date/date-to-date.py
+++ 241119/Date to Date/date-to-date.py
@@ -2,7 +2,7 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 m1, d1, m2, d2 = map(int, input().split())
 
-first_day = sum(num_of_days[:m1 + 1]) + d1
-second_day = sum(num_of_days[:m2 + 1]) + d2
+first_day = sum(num_of_days[:m1]) + d1
+second_day = sum(num_of_days[:m2]) + d2
 
-print(second_day - first_day)
+print(second_day - first_day + 1)
