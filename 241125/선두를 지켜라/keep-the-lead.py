a724a99172341b100fe36533add106f2407237c6
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-11-25 23:09:20 +0900

    Title: 선두를 지켜라, Time: 84ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 241125/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+
+A = [0]
+B = [0]
+
+def record_moving(n, arr):
+    for _ in range(n):
+        v, t = map(int, input().split())
+        for _ in range(t):
+            arr.append(arr[-1] + v)
+
+record_moving(N, A)
+record_moving(M, B)
+
+is_changed = 0
+leader = ""
+
+for i in range(1, len(A)):
+    if not len(leader) and A[i] != B[i]:
+        leader = "A" if A[i] > B[i] else "B"
+    elif leader == "B" and A[i] > B[i]:
+        is_changed += 1
+        leader = "A"
+    elif leader == "A" and A[i] < B[i]:
+        is_changed += 1
+        leader = "B"
+
+print(is_changed)   
