21b9b95fa8524d4fa3ed031dd568b24cdde45480
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-11-27 23:05:55 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 56ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241127/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,28 @@
+N, K, P, T = map(int, input().split())
+result = [0] * (N + 1)
+shakes = [False] * 251
+result[P] = K + 1
+
+for i in range(T):
+    t, x, y = map(int, input().split())
+    shakes[t] = [x, y]
+
+for shake in shakes:
+    if not shake:
+        continue
+    x, y = shake[0], shake[1]
+    x_count, y_count = result[x], result[y]
+    if x_count and y_count:
+        continue
+    elif x_count > 1:
+        result[x] -= 1
+        result[y] = K + 1
+    elif y_count > 1:
+        result[y] -= 1
+        result[x] = K + 1
+
+for i in range(1, len(result)):
+    if result[i]:
+        print('1', end="")
+    else:
+        print('0', end="")
