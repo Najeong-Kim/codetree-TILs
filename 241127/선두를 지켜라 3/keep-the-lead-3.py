20f38e32d8eafd04d76cc95faa35f17dac60e818
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-11-27 23:19:13 +0900

    Title: 선두를 지켜라 3, Time: 84ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 241127/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,32 @@
+N, M = map(int, input().split())
+
+A = [0]
+B = [0]
+
+def record_moving(n, arr):
+    for _ in range(n):
+        v, t = map(int, input().split())
+        for _ in range(t):
+            arr.append(arr[-1] + v)
+
+record_moving(N, A)
+record_moving(M, B)
+
+is_changed = 0
+leader = "C"
+
+for i in range(len(A)):
+    if leader == "C" and A[i] != B[i]:
+        is_changed += 1
+        leader = "A" if A[i] > B[i] else "B"
+    elif leader != "C" and A[i] == B[i]:
+        is_changed += 1
+        leader = "C"
+    elif leader == "B" and A[i] > B[i]:
+        is_changed += 2
+        leader = "A"
+    elif leader == "A" and A[i] < B[i]:
+        is_changed += 2
+        leader = "B"
+
+print(is_changed)   
