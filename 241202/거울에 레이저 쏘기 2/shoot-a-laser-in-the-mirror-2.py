17056fd61608bce6098798e5f153a7eb7febdddf
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-12-02 23:31:14 +0900

    Title: 거울에 레이저 쏘기 2, Time: 52ms, Memory: 17MB, Status: Passed - CodeTree

--- 241202/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 241202/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -14,15 +14,14 @@ count = 0
 dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
 x, y = 0, 0
 d = (K - 1) // N
-
 if d == 0:
     x, y = 1, K
 elif d == 1:
     x, y = K - N, N
 elif d == 2:
-    x, y = N, 3 * N - K
+    x, y = N, 3 * N - K + 1
 elif d == 3:
-    x, y = 4 * N - K, 1
+    x, y = 4 * N - K + 1, 1
 
 def in_range(x, y):
     return 1 <= x <= N and 1 <= y <= N
