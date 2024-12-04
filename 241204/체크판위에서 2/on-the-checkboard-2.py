e65ed50952b2ac608962dbc8509b4107f8052105
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-12-04 22:28:57 +0900

    Title: 체크판위에서 2, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 241204/체크판위에서 2/on-the-checkboard-2.py
+++ 241204/체크판위에서 2/on-the-checkboard-2.py
@@ -9,11 +9,13 @@ end_color = arr[R - 1][C - 1]
 
 count = 0
 
-for i in range(1, R - 2):
-    for j in range(1, C - 2):
-        for k in range(i + 1, R - 1):
-            for l in range(j + 1, C - 1):
-                if arr[i][j] == end_color and arr[k][l] == start_color:
-                    count += 1
-
-print(count)
+if start_color == end_color:
+    print(0)
+else:
+    for i in range(1, R - 2):
+        for j in range(1, C - 2):
+            for k in range(i + 1, R - 1):
+                for l in range(j + 1, C - 1):
+                    if arr[i][j] == end_color and arr[k][l] == start_color:
+                        count += 1
+    print(count)
