8a1fd410a5a2136e942f0501308422861a852544
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-12-05 23:01:39 +0900

    Title: 구간 중 최대 합, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241205/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+ans = 0
+
+for i in range(n - k):
+    sum_arr = 0
+    for j in range(i, i + k):
+        sum_arr += arr[j]
+
+    ans = max(ans, sum_arr)
+
+print(ans)
