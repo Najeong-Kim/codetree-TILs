84ecf48c94aace60d25d4427ef1adcd3ed2b003a
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-12-08 16:23:17 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241208/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,31 @@
+N = int(input())
+a, b, c = list(map(int, input().split()))
+a2, b2, c2 = list(map(int, input().split()))
+
+total = 0
+
+def is_satisfied(lock, key):
+    if key in get_list(lock):
+        return True
+    return False
+
+def get_list(a):
+    arr = []
+    arr.append(a - 1 + N if a - 1 < 1 else a - 1)
+    arr.append(a - 2 + N if a - 2 < 1 else a - 2)
+    arr.append(a)
+    arr.append(a + 1 + N if a + 1 > N else a + 1)
+    arr.append(a + 2 + N if a + 2 > N else a + 2)
+    return arr
+
+
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        for k in range(1, N + 1):
+            if is_satisfied(a, i) and is_satisfied(b, j) and is_satisfied(c, k):
+                total += 1
+                continue
+            if (abs(abs(a2 - i) + N) % N) <= 2 and (abs(abs(b2 - j) + N) % N) <= 2 and (abs(abs(c2 - k) + N) % N) <= 2:
+                total += 1
+
+print(total)
