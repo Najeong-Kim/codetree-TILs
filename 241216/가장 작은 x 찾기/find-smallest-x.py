f74cf12d042aacabf251da64decb64e6c495d53b
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-12-16 23:14:04 +0900

    Title: 가장 작은 x 찾기, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 241216/가장 작은 x 찾기/find-smallest-x.py
+++ 241216/가장 작은 x 찾기/find-smallest-x.py
@@ -1,10 +1,10 @@
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-for i in range(arr[0][0], arr[0][1] + 1):
+for i in range(10001):
     is_possible = True
     for j in range(n):
-        if arr[j][0] <= i * (2 ** (j + 1)) <= arr[j][1]:
+        if arr[j][0] <= (i * (2 ** (j + 1))) <= arr[j][1]:
             continue
         else:
             is_possible = False
