fef4079702db2e94f3c991fae447d851d502ad0d
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-12-22 00:50:07 +0900

    Title: 구간 잘 나누기, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241222/구간 잘 나누기/divide-sections-well.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+for i in range(sum(arr)):
+    count = 0
+    current = 0
+    is_possible = True
+    for j in range(len(arr)):
+        if current + arr[j] <= i:
+            current += arr[j]
+        else:
+            if current == 0:
+                is_possible = False
+            else:
+                count += 1
+                current = arr[j]
+    if count < m and is_possible:
+        print(i)
+        break
