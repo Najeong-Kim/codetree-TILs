d276f90e03ee9c9aa976c0a357ba288b0c15c2a1
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-12-27 23:27:28 +0900

    Title: 독서실의 거리두기 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241227/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 241227/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -14,20 +14,22 @@ for i in reversed(range(len(seats))):
         end = i
         break
 
-def get_distance(seats, isMax = True):
+real_seats = seats[start:end + 1]
+
+def get_distance(real_seats, isMax = True):
     now = 0
     distance = 0 if isMax else 1000
-    for i in range(1, len(seats)):
+    for i in range(1, len(real_seats)):
         now += 1
-        if seats[i] == '1':
+        if real_seats[i] == '1':
             distance = max(now, distance) if isMax else min(now, distance)
             now = 0
     return distance
 
 start_distance = start
 end_distance = len(seats) - 1 - end
-between_distance = math.floor(get_distance(seats) / 2)
-default_distance = get_distance(seats, False)
+between_distance = math.floor(get_distance(real_seats) / 2)
+default_distance = get_distance(real_seats, False)
 
 if start_distance >= between_distance or end_distance >= between_distance:
     print(max(min(start_distance, default_distance), min(end_distance, default_distance)))
