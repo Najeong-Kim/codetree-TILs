b9e70f481b16693ea05a0f7ccdd22a2ece10112c
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2024-12-29 12:46:19 +0900

    Title: 홀수 짝수의 묶음, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241229/홀수 짝수의 묶음/odd-even-bundle.py
@@ -0,0 +1,26 @@
+N = int(input())
+arr = list(map(int, input().split()))
+odd = 0
+even = 0
+isEvenOrder = True
+count = 0
+
+# 짝 == 홀
+# 짝 == 홀 + 1
+
+for elem in arr:
+    if elem % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+while True:
+    if odd == even:
+        print(N)
+        break
+    elif odd < even:
+        print(odd * 2 + 1)
+        break
+    else:
+        odd -= 2
+        even += 1
