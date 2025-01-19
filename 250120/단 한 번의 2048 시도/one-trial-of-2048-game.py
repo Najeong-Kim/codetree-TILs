d0c709f51f5f00207d92d8ad50ee5132248491a5
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-01-20 01:25:00 +0900

    Title: 단 한 번의 2048 시도, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250120/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -0,0 +1,65 @@
+# Read 4x4 grid
+grid = [list(map(int, input().split())) for _ in range(4)]
+
+# Read direction
+dir = input()
+
+# Write your code here!
+dirs = ['L', 'R', 'U', 'D']
+dx, dy = [0, 0, 1, -1], [-1, 1, 0, 0]
+
+if dir == 'L' or dir == 'R':
+    arr = []
+    for i in range(4):
+        prev = grid[i]
+        next = []
+        now = 0
+        repeat = [3, 2, 1, 0] if dir == 'R' else [0, 1, 2, 3]
+        for j in repeat:
+            if prev[j] != 0 and now == prev[j]:
+                next.append(now * 2)
+                now = 0
+            else:
+                if now != 0:
+                    next.append(now)
+                now = prev[j]
+        while len(next) < 4:
+            if now != 0:
+                next.append(now)
+                now = 0
+            else:
+                next.append(0)
+        if dir == 'R':
+            next = reversed(next)
+        arr.append(next)
+
+if dir == 'U' or dir == 'D':
+    arr = [[] for _ in range(4)]
+    for i in range(4):
+        prev = [grid[j][i] for j in range(4)]
+        next = []
+        now = 0
+        repeat = [3, 2, 1, 0] if dir == 'D' else [0, 1, 2, 3]
+        for j in repeat:
+            if prev[j] == 0:
+                continue
+            if now == prev[j]:
+                next.append(now * 2)
+                now = 0
+            else:
+                if now != 0:
+                    next.append(now)
+                now = prev[j]
+        while len(next) < 4:
+            if now != 0:
+                next.append(now)
+                now = 0
+            else:
+                next.append(0)
+        if dir == 'D':
+            next.reverse()
+        for j in range(4):
+            arr[j].append(next[j])
+
+for elem in arr:
+    print(*elem)
