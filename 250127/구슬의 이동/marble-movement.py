d91c11962b6a035ac408be9b2dcdc5b8c8ff43bf
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-01-27 22:08:10 +0900

    Title: 구슬의 이동, Time: 217ms, Memory: 26MB, Status: Passed - CodeTree

--- 250127/구슬의 이동/marble-movement.py
+++ 250127/구슬의 이동/marble-movement.py
@@ -1,12 +1,13 @@
 n, m, t, k = map(int, input().split())
 
-r, c, d, v = [], [], [], []
-for _ in range(m):
+r, c, d, v, num = [], [], [], [], []
+for i in range(m):
     ri, ci, di, vi = input().split()
     r.append(int(ri))
     c.append(int(ci))
     d.append(di)
     v.append(int(vi))
+    num.append(i + 1)
 
 dir, dx, dy = ['U', 'D', 'R', 'L'], [-1, 1, 0, 0], [0, 0, 1, -1]
 change = [1, 0, 3, 2]
@@ -19,29 +20,27 @@ for _ in range(t):
     for i in range(len(r)):
         nd = dir.index(d[i])
         x, y = r[i] - 1, c[i] - 1
-        for i in range(v[i]):
+        for _ in range(v[i]):
             nx, ny = x + dx[nd], y + dy[nd]
             if in_range(nx, ny):
                 x, y = nx, ny
             else:
                 nd = change[nd]
                 x, y = x + dx[nd], y + dy[nd]
-        board[x][y].append((dir[change[nd]], v[i]))
-    for i in range(n):
-        for j in range(n):
-            board[i][j].sort(lambda x: x[1])
+        board[x][y].append((dir[nd], v[i], num[i]))
     
-    nr, nc, nd, nv = [], [], [], []
+    nr, nc, nd, nv, nnum = [], [], [], [], []
     for i in range(n):
         for j in range(n):
-            board[i][j].sort(lambda x: x[1])
             if len(board[i][j]) > k:
+                board[i][j].sort(lambda x: (x[1], x[2]))
                 board[i][j] = board[i][j][len(board[i][j]) - k:]
             for l in board[i][j]:
                 nr.append(i + 1)
                 nc.append(j + 1)
                 nd.append(l[0])
                 nv.append(l[1])
-    r, c, d, v = nr, nc, nd, nv
+                nnum.append(l[2])
+    r, c, d, v, num = nr, nc, nd, nv, nnum
 
 print(len(r))
