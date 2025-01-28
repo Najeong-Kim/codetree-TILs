cb61e1ba22aa26bff19c902984d244f2900017cb
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-01-28 13:17:14 +0900

    Title: 벽이 없는 충돌 실험, Time: 3599ms, Memory: 25MB, Status: Passed - CodeTree

--- 250128/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 250128/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -11,8 +11,6 @@ for _ in range(T):
     N = int(input())
     x, y, w, d = [], [], [], []
 
-    check = [[[] for _ in range(4001)] for _ in range(4001)]
-
     for i in range(N):
         xi, yi, wi, di = input().split()
         nx, ny = (int(xi) + 1000) * 2, (int(yi) + 1000) * 2
@@ -20,25 +18,23 @@ for _ in range(T):
         y.append(ny)
         w.append(int(wi))
         d.append(di)
-        check[nx][ny].append(i)
 
     while time <= 4000:
         time += 1
+        check = {}
 
         for i in range(len(x)):
             if x[i] == None:
                 continue
             di = dir[d[i]]
             cx, cy = x[i] + dx[di], y[i] + dy[di]
-            check[x[i]][y[i]].remove(i)
             if not in_range(cx, cy):
                 x[i] = None
                 continue
-            now = list(filter(lambda x: x < i, check[cx][cy]))
-            if len(now):
-                exist = now[0]
+            if (cx, cy) in check:
+                exist = check[(cx, cy)]
                 if w[i] > w[exist] or (w[i] == w[exist] and i > exist):
-                    check[cx][cy][check[cx][cy].index(exist)] = i
+                    check[(cx, cy)] = i
                     x[i] = cx
                     y[i] = cy
                     x[exist] = None
@@ -46,7 +42,7 @@ for _ in range(T):
                     x[i] = None
                 result = time
             else:
-                check[cx][cy].append(i)
+                check[(cx, cy)] = i
                 x[i] = cx
                 y[i] = cy
 
