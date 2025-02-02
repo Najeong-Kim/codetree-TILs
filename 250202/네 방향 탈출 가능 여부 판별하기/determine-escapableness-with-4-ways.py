b568da04c531535ea5c00322980686a04a9b03ba
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-02-02 15:26:55 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 79ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 250202/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [[False] * m for _ in range(n)]
+deq = deque()
+visited[0][0] = True
+is_possible = False
+deq.append((0, 0))
+
+dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+
+def can_go(x, y):
+    if not (0 <= x < n and 0 <= y < m):
+        return False
+    if visited[x][y] or a[x][y] == 0:
+        return False
+    return True
+
+while len(deq):
+    x, y = deq.popleft()
+    if x == n - 1 and y == m - 1:
+        is_possible = True
+        break
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if can_go(nx, ny):
+            visited[nx][ny] = True
+            deq.append((nx, ny))
+
+if is_possible:
+    print(1)
+else:
+    print(0)
