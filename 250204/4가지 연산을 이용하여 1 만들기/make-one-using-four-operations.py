16781fc82b44f8db05bba3fe56aff60e5561b901
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-02-04 00:20:12 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 72ms, Memory: 32MB, Status: Wrong Answer - CodeTree

--- 250204/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 250204/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -2,12 +2,11 @@ from collections import deque
 
 N = int(input())
 
-step = [10 ** 6] * (10 ** 6 + 1)
+step = [0] * (10 ** 6 + 1)
 visited = [False] * (10 ** 6 + 1)
 deq = deque()
 visited[N] = True
 deq.append(N)
-step[N] = 0
 
 while len(deq):
     n = deq.popleft()
@@ -17,15 +16,15 @@ while len(deq):
     if n + 1 == 1 or n - 1 == 1 or (n % 3 == 0 and n // 3 == 1) or (n % 2 == 0 and n // 2 == 1):
         print(step[n] + 1)
         break
-    if n + 1 < N:
-        step[n + 1] = min(step[n + 1], step[n] + 1)
+    if n + 1 < N and step[n + 1] == 0:
+        step[n + 1] = step[n] + 1
         deq.append(n + 1)
-    if n - 1 >= 0:
-        step[n - 1] = min(step[n - 1], step[n] + 1)
+    if n - 1 >= 0 and step[n - 1] == 0:
+        step[n - 1] = step[n] + 1
         deq.append(n - 1)
-    if n % 3 == 0:
-        step[n // 3] = min(step[n // 3], step[n] + 1)
+    if n % 3 == 0 and step[n // 3] == 0:
+        step[n // 3] = step[n] + 1
         deq.append(n // 3)
-    if n % 2 == 0:
-        step[n // 2] = min(step[n // 2], step[n] + 1)
+    if n % 2 == 0 and step[n // 2] == 0:
+        step[n // 2] = step[n] + 1
         deq.append(n // 2)
