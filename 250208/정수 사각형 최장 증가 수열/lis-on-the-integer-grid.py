c23debf137a2ab10df73797df44a9b8d6064e3e3
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-02-08 09:39:33 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 261ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 250208/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 250208/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -3,26 +3,31 @@ sys.setrecursionlimit(10000)
 
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
-arr = [[1] * n for _ in range(n)]
+arr = [[0] * n for _ in range(n)]
 
 dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
-def run(x, y):
+def run(x, y, count):
     global arr
     global result
+    is_end = True
+    
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
         if in_range(nx, ny) and grid[x][y] < grid[nx][ny]:
-            if arr[nx][ny] < arr[x][y] + 1:
-                arr[nx][ny] = arr[x][y] + 1
-                run(nx, ny)
-
+            is_end = False
+            if arr[x][y]:
+                return arr[x][y] + 1
+            return run(nx, ny, count + 1)
+    if is_end:
+        return count
+        
 for i in range(n):
     for j in range(n):
-        run(i, j)
+        arr[i][j] = run(i, j, 1)
 
 result = 0
 for i in range(n):
