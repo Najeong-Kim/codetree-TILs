b4fb78f052d16af9e5246d8eea4294f2a06ed575
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-02-11 23:25:02 +0900

    Title: 2명의 도둑, Time: 39ms, Memory: 16MB, Status: Passed - CodeTree

--- 250211/2명의 도둑/two-thieves.py
+++ 250211/2명의 도둑/two-thieves.py
@@ -4,13 +4,26 @@ arr = [[0] * (n - m + 1) for _ in range(n)]
 
 def check_sum(x, y):
     now = weight[x][y:y + m]
-    now.sort()
     result = 0
-    count = 0
-    for elem in reversed(now):
-        if count + elem <= c:
-            count += elem
-            result += elem ** 2
+    check = 0
+
+    def pick_stone(index, count):
+        nonlocal result
+        nonlocal check
+        if index >= m:
+            result = max(result, check)
+            return
+        
+        pick_stone(index + 1, count)
+
+        if now[index] + count <= c:
+            count += now[index]
+            check += now[index] ** 2
+            pick_stone(index + 1, count)
+            count -= now[index]
+            check -= now[index] ** 2
+
+    pick_stone(0, 0)
     return result
 
 for i in range(n):
@@ -21,8 +34,8 @@ result = 0
 for i in range(n):
     for j in range(n - m + 1):
         for a in range(n):
-            for b in range(j, n - m + 1):
-                if i == a and b - j < m:
+            for b in range(n - m + 1):
+                if i == a and abs(b - j) < m:
                     continue
                 result = max(result, arr[i][j] + arr[a][b])
 
