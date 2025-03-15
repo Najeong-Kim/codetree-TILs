4ec57aed64ac73e18fec2fd49532026a7058518e
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-03-15 21:59:29 +0900

    Title: 최대 동전 거슬러주기, Time: 60ms, Memory: 17MB, Status: Passed - CodeTree

--- 250315/최대 동전 거슬러주기/max-coin-change.py
+++ 250315/최대 동전 거슬러주기/max-coin-change.py
@@ -1,16 +1,13 @@
 N, M = map(int, input().split())
 coin = list(map(int, input().split()))
-arr = [0] * (M + 1)
+arr = [-1] * (M + 1)
 arr[0] = 0
 
 for i in range(1, M + 1):
     for j in range(N):
         now = i - coin[j]
-        if now < 0:
+        if now < 0 or arr[now] == -1:
             continue
         arr[i] = max(arr[i], arr[now] + 1)
 
-if arr[M] != 0:
-    print(arr[M])
-else:
-    print(-1)
+print(arr[M])
