da55d00ca848834f9c63c375c519c27df537cb28
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-03-20 23:06:44 +0900

    Title: 계단 오르기 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250320/계단 오르기 2/climbing-stairs-2.py
+++ 250320/계단 오르기 2/climbing-stairs-2.py
@@ -1,12 +1,14 @@
 n = int(input())
 coin = [0] + list(map(int, input().split()))
 
-dp = [[0] * 3 for _ in range(n + 1)]
+dp = [[0] * 4 for _ in range(n + 1)]
 dp[1][1] = coin[1]
 
 for i in range(2, n + 1):
     dp[i][0] = dp[i - 2][0] + coin[i]
     dp[i][1] = max(dp[i - 2][1] + coin[i], dp[i - 1][0] + coin[i])
     dp[i][2] = max(dp[i - 2][2] + coin[i], dp[i - 1][1] + coin[i])
+    dp[i][3] = max(dp[i - 2][3] + coin[i], dp[i - 1][2] + coin[i])
+
 
 print(max(dp[n]))
