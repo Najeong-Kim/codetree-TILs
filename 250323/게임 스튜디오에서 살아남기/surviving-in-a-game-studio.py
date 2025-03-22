2e33c150c0e143d4f7a5186ba7202888a26fafd1
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-03-23 00:04:22 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 75ms, Memory: 19MB, Status: Wrong Answer - CodeTree

---
+++ 250323/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+dp = [[[0] * 3 for _ in range(3)] for _ in range(n)]
+dp[0][0][0] = 1
+dp[0][0][1] = 1
+dp[0][1][0] = 1
+
+for i in range(1, n):
+    for j in range(3):
+        for k in range(3):
+            dp[i][j][0] += dp[i - 1][j][k]
+    for j in range(1, 3):
+        for k in range(3):
+            dp[i][j][0] += dp[i - 1][j - 1][k]
+    for j in range(3):
+        for k in range(1, 3):
+            dp[i][j][k] += dp[i - 1][j][k - 1]
+
+result = 0
+for elem in dp[-1]:
+    result += sum(elem)
+print(result)
