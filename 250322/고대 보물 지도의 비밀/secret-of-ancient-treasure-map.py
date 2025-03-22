4171138945c3e1878365fcd7515c5ab9e0898200
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-03-22 23:12:53 +0900

    Title: 고대 보물 지도의 비밀, Time: 135ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 250322/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 250322/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -2,12 +2,24 @@ import math
 
 n, k = map(int, input().split())
 numbers = list(map(int, input().split()))
-# dp = [[-math.inf, 0] for _ in range(n + 1)]
-dp = [-math.inf] * (n + 1)
-dp[0] = [0, 0]
+dp = [[-math.inf] * (k + 1) for _ in range(n)]
+if numbers[0] < 0:
+    dp[0][0] = 0
+    dp[0][1] = numbers[0]
+else:
+    dp[0][0] = numbers[0]
 
-
-for i in range(n):
+for i in range(1, n):
     if numbers[i] < 0:
+        for j in range(1, k + 1):
+            dp[i][j] = dp[i - 1][j - 1] + numbers[i]
+    else:
+        dp[i][0] = max(dp[i - 1][0], numbers[i])
+        for j in range(1, k + 1):
+            dp[i][j] = dp[i - 1][j] + numbers[i]
 
-    dp[i + 1] = 
+result = -math.inf
+for elem in dp:
+    result = max(result, max(elem))
+
+print(result)
