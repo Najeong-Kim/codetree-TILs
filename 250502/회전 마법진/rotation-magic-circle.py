69ba33dfe8ae3375f96aec9c18a8f6f4e8577d3f
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-02 23:01:22 +0900

    Title: 회전 마법진, Time: 77ms, Memory: 19MB, Status: Passed - Codetree

--- 250502/회전 마법진/rotation-magic-circle.py
+++ 250502/회전 마법진/rotation-magic-circle.py
@@ -4,27 +4,19 @@ N = int(input())
 a = input()
 b = input()
 
-dp = [math.inf] * (9 * N + 10)
-dp[0] = 0
-
-active = {0}
+dp = [[math.inf] * 10 for _ in range(N + 1)]
+dp[0][0] = 0
 
 for i in range(N):
-    now_dp = [math.inf] * (9 * N + 10)
-    now_active = set()
-    for j in active:
-        if dp[j] == math.inf:
+    for j in range(10):
+        if dp[i][j] == math.inf:
             continue
         now = int(a[i]) + (j % 10)
         target = int(b[i])
         clockwise = (10 + now - target) % 10
         counterclockwise = (10 + target - now) % 10
-        now_dp[j] = min(now_dp[j], dp[j] + clockwise)
-        now_active.add(j)
-        now_dp[j + counterclockwise] = min(now_dp[j + counterclockwise], dp[j] + counterclockwise)
-        now_active.add(j + counterclockwise)
-    dp = now_dp
-    active = now_active
+        dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + clockwise)
+        dp[i + 1][(j + counterclockwise) % 10] = min(dp[i + 1][(j + counterclockwise) % 10], dp[i][j] + counterclockwise)
 
-print(min(dp))
+print(min(dp[-1]))
     
