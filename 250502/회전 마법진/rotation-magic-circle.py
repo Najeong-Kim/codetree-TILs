a57d814cbbfe384759e1aa843ef25566616a03c7
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-02 22:54:54 +0900

    Title: 회전 마법진, Time: 1096ms, Memory: 25MB, Status: Time Limit Exceed - Codetree

--- 250502/회전 마법진/rotation-magic-circle.py
+++ 250502/회전 마법진/rotation-magic-circle.py
@@ -7,9 +7,12 @@ b = input()
 dp = [math.inf] * (9 * N + 10)
 dp[0] = 0
 
+active = {0}
+
 for i in range(N):
     now_dp = [math.inf] * (9 * N + 10)
-    for j in range(9 * N):
+    now_active = set()
+    for j in active:
         if dp[j] == math.inf:
             continue
         now = int(a[i]) + (j % 10)
@@ -17,8 +20,11 @@ for i in range(N):
         clockwise = (10 + now - target) % 10
         counterclockwise = (10 + target - now) % 10
         now_dp[j] = min(now_dp[j], dp[j] + clockwise)
+        now_active.add(j)
         now_dp[j + counterclockwise] = min(now_dp[j + counterclockwise], dp[j] + counterclockwise)
+        now_active.add(j + counterclockwise)
     dp = now_dp
+    active = now_active
 
 print(min(dp))
     
