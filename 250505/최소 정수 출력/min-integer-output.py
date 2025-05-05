4c07fb8f83d28ee456ef7f96b61aaa713d28f349
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-05 22:13:27 +0900

    Title: 최소 정수 출력, Time: 259ms, Memory: 29MB, Status: Passed - Codetree

---
+++ 250505/최소 정수 출력/min-integer-output.py
@@ -0,0 +1,15 @@
+import heapq
+
+n = int(input())
+x = [int(input()) for _ in range(n)]
+
+heap = []
+
+for i in range(n):
+    if x[i] == 0:
+        if len(heap):
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, x[i])
