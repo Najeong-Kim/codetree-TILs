35a3bf2b02be8341dcd486501a56743f5c808c2e
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-05 22:53:52 +0900

    Title: 앞에서부터 삭제하기 2, Time: 90ms, Memory: 27MB, Status: Passed - Codetree

--- 250505/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 250505/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -7,8 +7,11 @@ heap = []
 heapq.heappush(heap, arr[-1])
 
 result = 0
+total = heap[0]
 for i in range(1, n):
-    heapq.heappush(heap, arr[-1-i])
-    result = max(result, sum(heap[1:]) / (len(heap) - 1))
+    now = arr[-1-i]
+    heapq.heappush(heap, now)
+    total += now
+    result = max(result, (total - heap[0]) / i)
 
 print("{:0.2f}".format(result))
