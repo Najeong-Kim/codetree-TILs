36d7ad759810ccc85d2ad3c57e8460290af2e62f
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-17 00:28:46 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 227ms, Memory: 58MB, Status: Passed - Codetree

--- 250517/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
+++ 250517/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -2,15 +2,14 @@ n, q = map(int, input().split())
 points = list(map(int, input().split()))
 ranges = [tuple(map(int, input().split())) for _ in range(q)]
 
-arr = [0] * (max(points) + 1)
+arr = [0] * (max(points) + 2)
 
 for point in points:
-    arr[point] += 1
+    arr[point + 1] += 1
 
 prefix_sum = [0]
 for i in range(1, len(arr)):
     prefix_sum.append(prefix_sum[-1] + arr[i])
-
 for r in ranges:
     start, end = r
-    print(prefix_sum[min(end, len(prefix_sum) - 1)] - prefix_sum[start - 1])
+    print(prefix_sum[min(end + 1, len(prefix_sum) - 1)] - prefix_sum[start])
