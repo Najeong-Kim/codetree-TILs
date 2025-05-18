d1e38f550bfd37ecc5d50c3eba6f07c0891793fc
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-18 23:20:24 +0900

    Title: 구간에 속한 문자의 개수, Time: 636ms, Memory: 47MB, Status: Passed - Codetree

--- 250518/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
+++ 250518/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -7,7 +7,11 @@ grid = [input() for _ in range(n)]
 queries = [tuple(map(int, input().split())) for _ in range(k)]
 
 # Please write your code here.
-prefix_sum = [[[0, 0, 0] for _ in range(m + 1)] for _ in range(n + 1)]
+prefix_sum = {
+    'a': [[0] * (m + 1) for _ in range(n + 1)],
+    'b': [[0] * (m + 1) for _ in range(n + 1)],
+    'c': [[0] * (m + 1) for _ in range(n + 1)]
+}
 
 words = 'abc'
 
@@ -15,12 +19,12 @@ for i in range(n):
     for j in range(m):
         for l in range(3):
             if grid[i][j] == words[l]:
-                prefix_sum[i + 1][j + 1][l] = prefix_sum[i + 1][j][l] + prefix_sum[i][j + 1][l] - prefix_sum[i][j][l] + 1
+                prefix_sum[words[l]][i + 1][j + 1] = prefix_sum[words[l]][i + 1][j] + prefix_sum[words[l]][i][j + 1] - prefix_sum[words[l]][i][j] + 1
             else:
-                prefix_sum[i + 1][j + 1][l] = prefix_sum[i + 1][j][l] + prefix_sum[i][j + 1][l] - prefix_sum[i][j][l]
+                prefix_sum[words[l]][i + 1][j + 1] = prefix_sum[words[l]][i + 1][j] + prefix_sum[words[l]][i][j + 1] - prefix_sum[words[l]][i][j]
 
 for i in range(k):
     r1, c1, r2, c2 = queries[i]
     for j in range(3):
-        print(prefix_sum[r2][c2][j] - prefix_sum[r2][c1 - 1][j] - prefix_sum[r1 - 1][c2][j] + prefix_sum[r1 - 1][c1 - 1][j], end=' ')
+        print(prefix_sum[words[j]][r2][c2] - prefix_sum[words[j]][r2][c1 - 1] - prefix_sum[words[j]][r1 - 1][c2] + prefix_sum[words[j]][r1 - 1][c1 - 1], end=' ')
     print()
