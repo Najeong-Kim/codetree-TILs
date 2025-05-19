4e540febb90a0c8701d70dfca5463a521b9becc5
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-19 21:17:40 +0900

    Title: 점 개수 세기, Time: 93ms, Memory: 19MB, Status: Wrong Answer - Codetree

---
+++ 250519/점 개수 세기/count-number-of-points.py
@@ -0,0 +1,14 @@
+from sortedcontainers import SortedSet
+
+n, q = map(int, input().split())
+points = list(map(int, input().split()))
+queries = [tuple(map(int, input().split())) for _ in range(q)]
+
+s = SortedSet()
+for i in range(n):
+    s.add(points[i])
+
+for i in range(q):
+    a, b = queries[i]
+    print(s.bisect_left(b) - s.bisect_left(a))
+    
