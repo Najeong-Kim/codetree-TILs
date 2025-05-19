bb8ed311d1c05979483e6a816d12db5274c1c98a
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-19 21:26:56 +0900

    Title: 점 개수 세기, Time: 1018ms, Memory: 47MB, Status: Time Limit Exceed - Codetree

--- 250519/점 개수 세기/count-number-of-points.py
+++ 250519/점 개수 세기/count-number-of-points.py
@@ -6,9 +6,7 @@ n, q = map(int, input().split())
 points = list(map(int, input().split()))
 queries = [tuple(map(int, input().split())) for _ in range(q)]
 
-s = SortedSet()
-for i in range(n):
-    s.add(points[i])
+s = SortedSet(points)
 
 for i in range(q):
     a, b = queries[i]
