921cca9a05b8951a01a171483fe9403400f4a46f
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-21 00:39:48 +0900

    Title: 중앙값, Time: 310ms, Memory: 27MB, Status: Passed - Codetree

--- 250521/중앙값/median.py
+++ 250521/중앙값/median.py
@@ -8,17 +8,12 @@ for _ in range(t):
     heap1 = []
     heap2 = []
     for i in range(m):
-        if i == 0:
-            heapq.heappush(heap1, arr[i])
-            print(heap1[0], end=' ')
-            continue
-        if i == 1:
-            heapq.heappush(heap1, arr[i])
-            heapq.heappush(heap2, -heapq.heappop(heap1))
-            continue
         if i % 2 == 0:
-            heapq.heappush(heap2, -arr[i])
-            heapq.heappush(heap1, -heapq.heappop(heap2))
+            if i == 0:
+                heapq.heappush(heap1, arr[i])
+            else:
+                heapq.heappush(heap2, -arr[i])
+                heapq.heappush(heap1, -heapq.heappop(heap2))
             print(heap1[0], end=' ')
         else:
             heapq.heappush(heap1, arr[i])
