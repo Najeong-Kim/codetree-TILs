75967a126b4870ea0b21f6b78dd0bd2ed8effb89
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-21 00:37:27 +0900

    Title: 중앙값, Time: 353ms, Memory: 28MB, Status: Passed - Codetree

--- 250521/중앙값/median.py
+++ 250521/중앙값/median.py
@@ -13,23 +13,14 @@ for _ in range(t):
             print(heap1[0], end=' ')
             continue
         if i == 1:
-            if arr[i] > heap1[0]:
-                heapq.heappush(heap1, arr[i])
-                heapq.heappush(heap2, -heapq.heappop(heap1))
-            else:
-                heapq.heappush(heap2, -arr[i])
+            heapq.heappush(heap1, arr[i])
+            heapq.heappush(heap2, -heapq.heappop(heap1))
             continue
         if i % 2 == 0:
-            if arr[i] > -heap2[0]:
-                heapq.heappush(heap1, arr[i])
-            else:
-                heapq.heappush(heap1, -heapq.heappop(heap2))
-                heapq.heappush(heap2, -arr[i])
+            heapq.heappush(heap2, -arr[i])
+            heapq.heappush(heap1, -heapq.heappop(heap2))
             print(heap1[0], end=' ')
         else:
-            if arr[i] > heap1[0]:
-                heapq.heappush(heap2, -heapq.heappop(heap1))
-                heapq.heappush(heap2, -arr[i])
-            else:
-                heapq.heappush(heap2, -arr[i])
+            heapq.heappush(heap1, arr[i])
+            heapq.heappush(heap2, -heapq.heappop(heap1))
     print()
