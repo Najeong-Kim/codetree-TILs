84c969ea04d9b798f5fb4d08e27d3fd454e2c09a
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-22 23:46:43 +0900

    Title: 최대 구간의 크기, Time: 754ms, Memory: 35MB, Status: Passed - Codetree

---
+++ 250522/최대 구간의 크기/size-of-largest-interval.py
@@ -0,0 +1,26 @@
+n = int(input())
+intervals = [tuple(map(int, input().split())) for _ in range(n)]
+
+arr = []
+for i in intervals:
+    arr.append((i[0], 1))
+    arr.append((i[1], -1))
+
+arr.sort()
+
+start = arr[0][0]
+is_connected = True
+count = 0
+result = 0
+
+for i in range(len(arr)):
+    point, value = arr[i]
+    count += value
+    if not is_connected:
+        start = point
+        is_connected = True
+    if count == 0:
+        result = max(result, point - start)
+        is_connected = False
+
+print(result)
