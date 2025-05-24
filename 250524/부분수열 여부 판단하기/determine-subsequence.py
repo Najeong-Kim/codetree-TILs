745a4f5613cae02302f049d271d82e74a9af2baf
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-05-24 23:40:55 +0900

    Title: 부분수열 여부 판단하기, Time: 117ms, Memory: 29MB, Status: Runtime Error - Codetree

---
+++ 250524/부분수열 여부 판단하기/determine-subsequence.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+ai = 0
+count = 0
+
+for i in range(m):
+    while ai + 1 < n and A[ai] != B[i]:
+        ai += 1
+    if A[ai] == B[i]:
+        count += 1
+        ai += 1
+
+if count == len(B):
+    print('Yes')
+else:
+    print('No')
