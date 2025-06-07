2a40143428e2d9af13e3209405fa6f3df9cda0d3
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-06-07 23:52:53 +0900

    Title: 삼 오 무, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250607/삼 오 무/three-five-moo.py
+++ 250607/삼 오 무/three-five-moo.py
@@ -2,14 +2,17 @@ n = int(input())
 
 left = 1
 right = 10 ** 18
+min_value = 10 ** 18
 
 while left <= right:
     mid = (left + right) // 2
     count = mid - (mid // 3) - (mid // 5) + (mid // 15)
     if count == n:
-        print(mid)
-        break
+        min_value = min(min_value, mid)
+        right = mid - 1
     elif count < n:
         left = mid + 1
     else:
         right = mid - 1
+
+print(min_value)
