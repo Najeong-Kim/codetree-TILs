b95fc88f9213fd079bb867b3b89424b374819224
NAJEONG KIM <73640737+Najeong-Kim@users.noreply.github.com>
2025-06-15 23:58:40 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 223ms, Memory: 25MB, Status: Wrong Answer - Codetree

---
+++ 250615/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+arr = []
+for i in range(N):
+    w, v = map(int, input().split())
+    arr.append([round(v / w, 4), w, v])
+
+arr.sort(lambda x: -x[0])
+
+result = 0
+index = 0
+bag = M
+while bag > 0:
+    p, w, v = arr[index]
+    if bag >= w:
+        result += v
+        index += 1
+        bag -= w
+    else:
+        result += p * bag
+        break
+
+print(f"{result:.3f}")
